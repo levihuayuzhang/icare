ec8a58f991a318b0f186d0dd19b91d80ae44ef51
Huayu Zhang <2721652z@student.gla.ac.uk>
2023-06-25 12:47:46 +0800

    remove nacos-server module

---
+++ patient-server/src/main/java/org/huayuzhang/patient/web/PatientApplication.java
@@ -0,0 +1,13 @@
+package org.huayuzhang.patient.web;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("org.huayuzhang.patient.mapper")
+@SpringBootApplication
+public class PatientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PatientApplication.class, args);
+    }
+}
