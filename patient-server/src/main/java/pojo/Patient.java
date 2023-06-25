ae426476389c89e6a5277db12b4b4985b91a97fb
Huayu Zhang <2721652z@student.gla.ac.uk>
2023-06-25 11:36:39 +0800

    add patient basic data field

---
+++ patient-server/src/main/java/pojo/Patient.java
@@ -0,0 +1,9 @@
+package pojo;
+import lombok.Data;
+@Data
+public class Patient {
+    private Long id; // patient ID
+    private String patientName; // patient name
+    private String phoneNum; // patient phone number
+    private String address; // patient address
+}
