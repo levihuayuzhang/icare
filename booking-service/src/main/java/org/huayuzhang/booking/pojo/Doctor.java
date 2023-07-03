dc92928c5f09bcdc1313eb3de36b9ce09c8da159
Huayu Zhang <2721652z@student.gla.ac.uk>
2023-07-03 14:27:57 +0800

    get Data field from other micro service

---
+++ booking-service/src/main/java/org/huayuzhang/booking/pojo/Doctor.java
@@ -0,0 +1,11 @@
+package org.huayuzhang.booking.pojo;
+
+import lombok.Data;
+
+@Data
+public class Doctor {
+    private Long id; // patient ID
+    private String docName; // patient name
+    private String phoneNum; // patient phone number
+    private String department; // patient address
+}
