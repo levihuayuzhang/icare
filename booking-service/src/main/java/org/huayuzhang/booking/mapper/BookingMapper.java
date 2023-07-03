dae8093e010ae610a410be7e232f59eaca62cbfb
Huayu Zhang <2721652z@student.gla.ac.uk>
2023-07-03 16:50:25 +0800

    change template to table booking

--- booking-service/src/main/java/org/huayuzhang/booking/mapper/BookingMapper.java
+++ booking-service/src/main/java/org/huayuzhang/booking/mapper/BookingMapper.java
@@ -4,6 +4,6 @@ import org.huayuzhang.booking.pojo.Booking;
 import org.apache.ibatis.annotations.Select;
 
 public interface BookingMapper {
-    @Select("select * from tb_order where id = #{id}")
-    Booking findById(Long id);
+    @Select("select * from tb_booking where bkID = #{bkID}")
+    Booking findById(Long bkID);
 }
