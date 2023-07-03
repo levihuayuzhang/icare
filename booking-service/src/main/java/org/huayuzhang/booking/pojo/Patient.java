6d38211602019e6b86f7356e35b26f7c0db398be
Huayu Zhang <2721652z@student.gla.ac.uk>
2023-07-03 14:52:24 +0800

    add gender for patient

--- booking-service/src/main/java/org/huayuzhang/booking/pojo/Patient.java
+++ booking-service/src/main/java/org/huayuzhang/booking/pojo/Patient.java
@@ -6,6 +6,7 @@ import lombok.Data;
 public class Patient {
     private Long id; // patient ID
     private String patientName; // patient name
+    private Integer gender; // patient gender (biological) (0 for female, 1 for male)
     private String phoneNum; // patient phone number
     private String address; // patient address
 }
--- patient-service/src/main/java/org/huayuzhang/patient/pojo/Patient.java
+++ patient-service/src/main/java/org/huayuzhang/patient/pojo/Patient.java
@@ -4,8 +4,8 @@ import lombok.Data;
 @Data
 public class Patient {
     private Long id; // patient ID
-    private Integer gender; // patient gender (biological) (0 for female, 1 for male)
     private String patientName; // patient name
+    private Integer gender; // patient gender (biological) (0 for female, 1 for male)
     private String phoneNum; // patient phone number
     private String address; // patient address
 }
