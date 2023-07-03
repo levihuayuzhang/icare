6aac4eafa11e76784d4c553e643e664c18c7d9d4
Huayu Zhang <2721652z@student.gla.ac.uk>
2023-07-03 14:52:11 +0800

    add gender for patient in sql script/database

--- examples/SQLs/cloud-patient.sql
+++ examples/SQLs/cloud-patient.sql
@@ -11,6 +11,7 @@ DROP TABLE IF EXISTS `tb_patient`;
 CREATE TABLE `tb_patient`  (
                             `id` bigint(20) NOT NULL AUTO_INCREMENT,
                             `patientName` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'patient name',
+                            `gender` bigint(20) NOT NULL,
                             `phoneNum` varchar(35) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'patient phone number',
                             `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'patient address',
                             PRIMARY KEY (`id`) USING BTREE,
@@ -18,11 +19,11 @@ CREATE TABLE `tb_patient`  (
                             ) ENGINE = InnoDB AUTO_INCREMENT = 109 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 /* add records */
-INSERT INTO `tb_patient` VALUES (1, 'alice','01111 111111', 'Glasgow');
-INSERT INTO `tb_patient` VALUES (2, 'bob', '02222 222222', 'New York');
-INSERT INTO `tb_patient` VALUES (3, 'john', '03333 333333', 'Seoul');
-INSERT INTO `tb_patient` VALUES (4, 'steven', '04444 444444', 'Tokyo');
-INSERT INTO `tb_patient` VALUES (5, 'thomas', '05555 555555', 'Beijing');
-INSERT INTO `tb_patient` VALUES (6, 'franklin','06666 666666', 'London');
+INSERT INTO `tb_patient` VALUES (1,'alice', 0, '01111 111111', 'Glasgow');
+INSERT INTO `tb_patient` VALUES (2, 'bob', 1,  '02222 222222', 'New York');
+INSERT INTO `tb_patient` VALUES (3, 'john', 1,  '03333 333333', 'Seoul');
+INSERT INTO `tb_patient` VALUES (4, 'steven', 1,  '04444 444444', 'Tokyo');
+INSERT INTO `tb_patient` VALUES (5, 'thomas', 1,  '05555 555555', 'Beijing');
+INSERT INTO `tb_patient` VALUES (6, 'franklin', 1, '06666 666666', 'London');
 
 SET FOREIGN_KEY_CHECKS = 1;
