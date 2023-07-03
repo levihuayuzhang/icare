53503ed80784322d95a494fb6c29d409b78c8e2c
Huayu Zhang <2721652z@student.gla.ac.uk>
2023-07-03 17:51:36 +0800

    add param annotation

--- booking-service/src/main/java/org/huayuzhang/booking/mapper/BookingMapper.java
+++ booking-service/src/main/java/org/huayuzhang/booking/mapper/BookingMapper.java
@@ -1,9 +1,10 @@
 package org.huayuzhang.booking.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.huayuzhang.booking.pojo.Booking;
 import org.apache.ibatis.annotations.Select;
 
 public interface BookingMapper {
     @Select("select * from tb_booking where bkID = #{bkID}")
-    Booking findById(Long bkID);
+    Booking findById(@Param("bkID") Long bkID);
 }
