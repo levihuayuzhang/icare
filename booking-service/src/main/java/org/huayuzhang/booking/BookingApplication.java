65eb396f7acad0db3e427cf378a7e70afe6836e7
Huayu Zhang <2721652z@student.gla.ac.uk>
2023-07-03 20:10:54 +0800

    enabling feign in booking service

--- booking-service/src/main/java/org/huayuzhang/booking/BookingApplication.java
+++ booking-service/src/main/java/org/huayuzhang/booking/BookingApplication.java
@@ -1,14 +1,17 @@
 package org.huayuzhang.booking;
 
+import org.huayuzhang.booking.config.DefaultFeignConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @MapperScan("org.huayuzhang.booking.mapper")
 @SpringBootApplication
+@EnableFeignClients(defaultConfiguration = DefaultFeignConfiguration.class)
 public class BookingApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookingApplication.class, args);
