60c6ba894258532d9be2a708880662a93ab2b0e0
Huayu Zhang <2721652z@student.gla.ac.uk>
2023-07-03 17:51:21 +0800

    add service annotation

--- booking-service/src/main/java/org/huayuzhang/booking/service/BookingService.java
+++ booking-service/src/main/java/org/huayuzhang/booking/service/BookingService.java
@@ -3,7 +3,9 @@ package org.huayuzhang.booking.service;
 import org.huayuzhang.booking.mapper.BookingMapper;
 import org.huayuzhang.booking.pojo.Booking;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookingService {
     @Autowired
     private BookingMapper bookingMapper;
