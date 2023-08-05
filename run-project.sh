0f70db4bc54eb36aa75a7008253be25f22eef33d
Huayu Zhang <2721652z@student.gla.ac.uk>
2023-08-05 22:10:41 +0800

    microservices can be deployed and run use script at root directory now

--- run-project.sh
+++ run-project.sh
@@ -5,3 +5,9 @@ cp ./booking-service/target/app.jar ./examples/docker/image/booking-service/
 cp ./doctor-service/target/app.jar ./examples/docker/image/doctor-service/
 cp ./patient-service/target/app.jar ./examples/docker/image/patient-service/
 cp ./gateway/target/app.jar ./examples/docker/image/gateway/
+
+cd ./examples/docker
+docker-compose -f clusters.yaml up --build -d
+
+#sleep 90
+#docker-compose -f clusters.yaml restart gateway patientservice doctorservice bookingservice
