03106c7a85c9c2eac411ee36d9c6eac74df70a36
Huayu Zhang <2721652z@student.gla.ac.uk>
2023-08-05 20:20:49 +0800

    scripts and Dockerfile for building images (each microservice)

--- run-project.sh
+++ run-project.sh
@@ -1,7 +1,7 @@
 # run in root directory
 mvn clean package
 
-cp ./booking-service/target/app.jar ./
-cp ./doctor-service/target/app.jar ./
-cp ./patient-service/target/app.jar ./
-cp ./gateway-service/target/app.jar ./
+cp ./booking-service/target/app.jar ./examples/docker/image/booking-service/
+cp ./doctor-service/target/app.jar ./examples/docker/image/doctor-service/
+cp ./patient-service/target/app.jar ./examples/docker/image/patient-service/
+cp ./gateway/target/app.jar ./examples/docker/image/gateway/
