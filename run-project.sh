c613808e3cff7fb9fdbce35b6518dad395095a06
Huayu Zhang <2721652z@student.gla.ac.uk>
2023-08-18 21:35:46 +0800

    update scripts for testing and building project

--- run-project.sh
+++ run-project.sh
@@ -1,14 +1,19 @@
-# run in root directory
+#!/bin/sh
+
+# run this script in root directory
 # sh run-project.sh
 
+# clean maven project builds and recompiling & repackaging
 mvn clean package
 
+# copy jar packages into image prepare directory
 cp ./patient-service/target/app.jar ./examples/docker/image/patient-service/
 cp ./doctor-service/target/app.jar ./examples/docker/image/doctor-service/
 cp ./payment-service/target/app.jar ./examples/docker/image/payment-service/
 cp ./booking-service/target/app.jar ./examples/docker/image/booking-service/
 cp ./gateway/target/app.jar ./examples/docker/image/gateway/
 
+# enter into Docker directory, build image and start up container clusters
 cd ./examples/docker
 docker-compose -f clusters.yaml up --build -d
 
--- test-project.sh
+++ test-project.sh
@@ -1,2 +1,12 @@
+#!/bin/sh
+
+# use for start up graphic interface of Jmeter
 #jmeter -t ./testing/Jmeter/test-plan.jmx
+
+# use for no graphic testing and generate report
+# -n for no graphic
+# -t for jmx project file
+# -l log file
+# -e for export report
+# -o for report output location
 jmeter -n -t ./testing/Jmeter/test-plan.jmx -l ./testing/Jmeter/result/result.txt -e -o ./testing/Jmeter/webreport
